e414d3028a8e8e37800a9adb12ef42db13d7436e
Dmitry <medtex03@gmail.com>
2023-03-07 08:36:53 +0800

    filterSet added to animal list view

--- animal/views.py
+++ animal/views.py
@@ -1,19 +1,20 @@
 from rest_framework.response import Response
 from rest_framework import generics, status
-from rest_framework.exceptions import ValidationError
 
 from .models import Animal
+from .filters import AnimalFilter
 from .serializers import AnimalSerializer
 from core.exceptions import *
 from core.utils import validate_dict_number
 from core.permissions import CustomPermission
 
 from animal_type.models import AnimalType
-from animal_location.models import AnimalLocation 
 from location.models import Location
 from account.models import Account
 
-import datetime
+import datetime, logging
+logger = logging.getLogger(__name__)
+from django_filters import rest_framework as filters
 
 # POST on http://localhost:8000/animals
 class CreateAnimal(generics.CreateAPIView):
@@ -98,65 +99,8 @@ GET on http://localhost:8000/animals/search
 Additional parmaetrs: firstname, lastName, email, from, size
 """
 class AnimalList(generics.ListAPIView):
-    queryset = Animal.objects.all()
+    queryset = Animal.objects.all().order_by('id')
     serializer_class = AnimalSerializer
     permission_classes = (CustomPermission,)
-
-    def get_queryset(self):
-        
-        # Define all query_params
-        startDateTime = self.request.query_params.get('startDateTime', None) 
-        endDateTime = self.request.query_params.get('endDateTime', None) 
-        chipperId = self.request.query_params.get('chipperId', None) 
-        chippingLocationId = self.request.query_params.get('chippingLocationId', None)
-        lifeStatus = self.request.query_params.get('lifeStatus', None)
-        gender = self.request.query_params.get('gender', None)
-        from_ = self.request.query_params.get('from', '0')
-        size = self.request.query_params.get('size', '10')
-
-        if(not (from_.isnumeric() and size.isnumeric())):
-            raise BadRequestException('from or size is not numeric')
-
-        from_, size = int(from_), int(size)
-
-        if(from_ < 0 or size <= 0):
-            raise BadRequestException('from and size should be > 0 and not equals null')
-
-        '''
-            TODO: Optimize if statments
-            START
-        '''
-
-        filter = {}
-        
-        if startDateTime != None and endDateTime != None:
-            filter['chippingDateTime__range'] = (startDateTime, endDateTime)
-
-        elif startDateTime != None:
-            filter['chippingDateTime__gte'] = startDateTime
-        
-        elif endDateTime != None:
-            filter['chippingDateTime__lte'] = endDateTime
-
-
-        if(chipperId != None):
-            filter['chipperId'] = chipperId
-            
-        if(chippingLocationId != None):
-            filter['chippingLocationId'] = chippingLocationId
-            
-        if(lifeStatus != None):
-            filter['lifeStatus'] = lifeStatus
-            
-        if(gender != None):
-            filter['gender'] = gender
-
-        '''
-            TODO: Optimize if statments
-            END
-        '''
-
-        # Filter by query_params
-        queryset = Animal.objects.filter(**filter).order_by('-id').reverse()
-
-        return queryset[from_:from_+size]
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AnimalFilter
